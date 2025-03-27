f2b985243eb9d44cbc3448af48af56661f2985a0
AthulyaS123 <athulyasworld@yahoo.com>
2025-03-27 14:43:53 -0500

    front end for landing, filter, lookbook, account, and outfit screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ClosetScreen from "./Screens/closetscreen.js";
 import MixAndMatchScreen from "./Screens/mixandmatchscreen.js";
 import SwipeScreen from "./Screens/swipescreen.js";
+import LandingScreen from "./Screens/landingscreen.js";
+import CreateAccountScreen from "./Screens/createaccountscreen.js";
+import FilterScreen from "./Screens/filterscreen.js";
+import LookbookScreen from "./Screens/lookbookscreen.js";
+import OutfitScreen from "./Screens/outfitscreen.js";
+
+
+
 
 
 
@@ -11,9 +19,14 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path="/" element={<SwipeScreen />} /> */}
-          {/* <Route path="/closet" element={<ClosetScreen />} /> */}
+          <Route path="/" element={<SwipeScreen />} />
+          <Route path="/closet" element={<ClosetScreen />} />
           <Route path="/mix-and-match" element={<MixAndMatchScreen />} />
+          <Route path="/landing" element={<LandingScreen />} />
+          <Route path="/create-account" element={<LandingScreen />} />
+          <Route path="/filter" element={<FilterScreen />} />
+          <Route path="/lookbook" element={<LookbookScreen />} />
+          <Route path="/outfit" element={<OutfitScreen />} />
         </Routes>
       </div>
     </Router> 
---
+++ my-app/src/Screens/lookbookscreen.js
@@ -0,0 +1,49 @@
+import { FunctionComponent } from 'react';
+import styles from './lookbookscreen.module.css';
+
+
+const Lookbook = () => {
+  	return (
+    		<div className={styles.lookbook}>
+      			<div className={styles.lookbookChild} />
+      			<div className={styles.yourLookbook}>Your Lookbook</div>
+      			<img className={styles.cutIcon} alt="" src="Cut.png" />
+      			<img className={styles.wardrobeIcon} alt="" src="Wardrobe.png" />
+      			<img className={styles.tuneIcon} alt="" src="Tune.png" />
+      			<div className={styles.ellipseParent}>
+        				<div className={styles.groupChild} />
+        				<div className={styles.div}>?</div>
+          					</div>
+          					<img className={styles.heartIcon} alt="" src="Heart.png" />
+          					<div className={styles.closet}>Closet</div>
+          					<div className={styles.swipe}>Swipe</div>
+          					<div className={styles.mixMatch}>{`Mix & Match`}</div>
+          					<div className={styles.lookbook1}>Lookbook</div>
+          					<img className={styles.openBookIcon} alt="" src="Open Book.png" />
+          					<div className={styles.save}>Save</div>
+          					<div className={styles.lookbookItem} />
+          					<img className={styles.image8Icon} alt="" src="image 8.png" />
+          					<img className={styles.image12Icon} alt="" src="image 12.png" />
+          					<img className={styles.image11Icon} alt="" src="image 11.png" />
+          					<div className={styles.lookbookInner} />
+          					<img className={styles.image7Icon} alt="" src="image 7.png" />
+          					<div className={styles.rectangleDiv} />
+          					<div className={styles.lookbookChild1} />
+          					<img className={styles.image2Icon} alt="" src="image 2.png" />
+          					<div className={styles.lookbookChild2} />
+          					<div className={styles.lookbookChild3} />
+          					<img className={styles.image10Icon} alt="" src="image 10.png" />
+          					<img className={styles.image14Icon} alt="" src="image 14.png" />
+          					<img className={styles.image16Icon} alt="" src="image 16.png" />
+          					<img className={styles.image15Icon} alt="" src="image 15.png" />
+          					<img className={styles.image19Icon} alt="" src="image 19.png" />
+          					<img className={styles.downloadingUpdatesIcon} alt="" src="Downloading Updates.png" />
+          					<img className={styles.downloadingUpdatesIcon1} alt="" src="Downloading Updates.png" />
+          					<img className={styles.downloadingUpdatesIcon2} alt="" src="Downloading Updates.png" />
+          					<img className={styles.downloadingUpdatesIcon3} alt="" src="Downloading Updates.png" />
+          					<div className={styles.user} />
+          					</div>);
+        				};
+        				
+        				export default Lookbook;
+        				
