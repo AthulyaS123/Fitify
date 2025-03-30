c0f52f239f32eb467239464a424826877191f154
Sarah Chen <sarah.jchen6@gmail.com>
2025-03-30 17:16:20 -0500

    added bootstrap styling to mixandmatch and lookbook screens

--- my-app/src/Screens/lookbookscreen.js
+++ my-app/src/Screens/lookbookscreen.js
@@ -1,49 +1,117 @@
 import { FunctionComponent } from 'react';
-import styles from './lookbookscreen.module.css';
+import { Link } from 'react-router-dom';
+import preficon from './Icons/preficon.png';
+import hearticon from './Icons/hearticon.png';
+import fitsicon from './Icons/fitsicon.png';
+import mixandmatchicon from './Icons/mixandmatchicon.png';
+import closeticon from './Icons/closeticon.png';
+import usericon from './Icons/user.png';
+import questionicon from './Icons/question.png';
+
+// Component imports
+import SavedOutfit from './Components/SavedOutfit';
+
+// Import ALL png files from Clothes folder
+// access images using Clothes.top1, etc.
+import * as Clothes from './Clothes';
+
+import './lookbookscreen.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Lookbook = () => {
   	return (
-    		<div className={styles.lookbook}>
-      			<div className={styles.lookbookChild} />
-      			<div className={styles.yourLookbook}>Your Lookbook</div>
-      			<img className={styles.cutIcon} alt="" src="Cut.png" />
-      			<img className={styles.wardrobeIcon} alt="" src="Wardrobe.png" />
-      			<img className={styles.tuneIcon} alt="" src="Tune.png" />
-      			<div className={styles.ellipseParent}>
-        				<div className={styles.groupChild} />
-        				<div className={styles.div}>?</div>
-          					</div>
-          					<img className={styles.heartIcon} alt="" src="Heart.png" />
-          					<div className={styles.closet}>Closet</div>
-          					<div className={styles.swipe}>Swipe</div>
-          					<div className={styles.mixMatch}>{`Mix & Match`}</div>
-          					<div className={styles.lookbook1}>Lookbook</div>
-          					<img className={styles.openBookIcon} alt="" src="Open Book.png" />
-          					<div className={styles.save}>Save</div>
-          					<div className={styles.lookbookItem} />
-          					<img className={styles.image8Icon} alt="" src="image 8.png" />
-          					<img className={styles.image12Icon} alt="" src="image 12.png" />
-          					<img className={styles.image11Icon} alt="" src="image 11.png" />
-          					<div className={styles.lookbookInner} />
-          					<img className={styles.image7Icon} alt="" src="image 7.png" />
-          					<div className={styles.rectangleDiv} />
-          					<div className={styles.lookbookChild1} />
-          					<img className={styles.image2Icon} alt="" src="image 2.png" />
-          					<div className={styles.lookbookChild2} />
-          					<div className={styles.lookbookChild3} />
-          					<img className={styles.image10Icon} alt="" src="image 10.png" />
-          					<img className={styles.image14Icon} alt="" src="image 14.png" />
-          					<img className={styles.image16Icon} alt="" src="image 16.png" />
-          					<img className={styles.image15Icon} alt="" src="image 15.png" />
-          					<img className={styles.image19Icon} alt="" src="image 19.png" />
-          					<img className={styles.downloadingUpdatesIcon} alt="" src="Downloading Updates.png" />
-          					<img className={styles.downloadingUpdatesIcon1} alt="" src="Downloading Updates.png" />
-          					<img className={styles.downloadingUpdatesIcon2} alt="" src="Downloading Updates.png" />
-          					<img className={styles.downloadingUpdatesIcon3} alt="" src="Downloading Updates.png" />
-          					<div className={styles.user} />
-          					</div>);
-        				};
+		<div className="container">
+			{/* Header */}
+			<div className="top-content row align-items-center">
+				<div className="col-1">
+					<img className="d-block mx-auto" src={preficon} alt="Preferences" width="30" />
+				</div>
+	
+				<div className="col-1">
+					<img className="d-block mx-auto" src={questionicon} alt="Info" width="30" />
+				</div>
+	
+				<div className="col-8">
+					<p className="pageheader">Lookbook</p>
+				</div>
+	
+				<div className="col-2">
+					<img className="d-block mx-auto" src={usericon} alt="Profile" width="40" />
+				</div>
+			</div>
+
+		
+			{/* Saved Outfits Content */}
+			<div className="row outfit-row">
+				<div className="col">
+					<SavedOutfit
+						top={Clothes.top1}
+						bottom={Clothes.pant1}
+						shoe={Clothes.shoe1}
+						/>
+				</div>
+				<div className="col">
+					<SavedOutfit
+						top={Clothes.redtop}
+						bottom={Clothes.pant2}
+						shoe={Clothes.shoe2}/>
+				</div>	
+			</div>
+
+			<div className="row outfit-row">
+				<div className="col">
+					<SavedOutfit
+						top={Clothes.top2}
+						bottom={Clothes.pant3}
+						shoe={Clothes.shoe3}/>
+				</div>
+				<div className="col">
+					<SavedOutfit
+						top={Clothes.top3}
+						bottom={Clothes.pant4}
+						shoe={Clothes.shoe4}/>
+				</div>
+			</div>
+
+	
+
+		
+
+			{/* Bottom Navigation */}
+			<div className="row navbar fixed-bottom">
+			<div className="col text-center">
+				<Link to="/" className="nav-link-current">
+				<img src={hearticon} alt="Swipe" width="40" />
+				<div>Swipe</div>
+				</Link>
+			</div>
+	
+			<div className="col text-center">
+				<Link to="/closet" className="nav-link">
+				<img src={closeticon} alt="Closet" width="40" />
+				<div>Closet</div>
+				</Link>
+			</div>
+			<div className="col text-center">
+				<Link to="/mix-and-match" className="nav-link">
+				<img src={mixandmatchicon} alt="Mix & Match" width="40" />
+				<div className="nav-link">Mix & Match</div>
+				</Link>
+			</div>
+			<div className="col text-center">
+				<Link to="/lookbook" className="nav-link">
+				<img src={fitsicon} alt="Lookbook" width="40" />
+				<div>Lookbook</div>
+				</Link>
+			</div>
+			</div>
+		</div>
+
+
+
+		);
+    };
         				
-        				export default Lookbook;
+export default Lookbook;
         				
