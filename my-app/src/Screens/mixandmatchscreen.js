34faa9052e146cb20916cf48dd2ed097ca6d15eb
mansimadhani <madhanimansi56@gmail.com>
2025-03-31 21:01:03 -0500

    compiled changes from mix and match; reuploaded clothing images

--- my-app/src/Screens/mixandmatchscreen.js
+++ my-app/src/Screens/mixandmatchscreen.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
-import { FunctionComponent } from 'react';
+import { useState } from 'react';
+import { useClothing } from './clothingprovider';
+
 import preficon from './Icons/preficon.png';
 import hearticon from './Icons/whiteheart.png';
 import fitsicon from './Icons/fitsicon.png';
@@ -11,87 +13,99 @@ import leftarrow from './Icons/arrowlefticon.png';
 import rightarrow from './Icons/arrowrighticon.png';
 
 import './mixandmatchscreen.css';
-// Import ALL png files from Clothes folder
-// access images using Clothes.top1, etc.
-import * as Clothes from './Clothes';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MixMatch = () => {
+  const { likedItems } = useClothing();
+
+  // Filter by category
+  const tops = likedItems.filter((item) => item.category === 'top');
+  const pants = likedItems.filter((item) => item.category === 'bottom');
+  const shoes = likedItems.filter((item) => item.category === 'shoe');
+
+  // Indices for navigation
+  const [topIndex, setTopIndex] = useState(0);
+  const [pantIndex, setPantIndex] = useState(0);
+  const [shoeIndex, setShoeIndex] = useState(0);
+
+  // Handlers
+  const handleNext = (type) => {
+    if (type === 'top' && tops.length > 0) setTopIndex((topIndex + 1) % tops.length);
+    if (type === 'pant' && pants.length > 0) setPantIndex((pantIndex + 1) % pants.length);
+    if (type === 'shoe' && shoes.length > 0) setShoeIndex((shoeIndex + 1) % shoes.length);
+  };
+
+  const handlePrev = (type) => {
+    if (type === 'top' && tops.length > 0) setTopIndex((topIndex - 1 + tops.length) % tops.length);
+    if (type === 'pant' && pants.length > 0) setPantIndex((pantIndex - 1 + pants.length) % pants.length);
+    if (type === 'shoe' && shoes.length > 0) setShoeIndex((shoeIndex - 1 + shoes.length) % shoes.length);
+  };
+
   return (
-	<div className="container">
-		{/* Header */}
-		<div className="top-content row align-items-center">
-			<div className="col-1">
-				<img className="d-block mx-auto" src={preficon} alt="Preferences" width="30" />
-			</div>
+    <div className="container">
+      {/* Header */}
+      <div className="top-content row align-items-center">
+        <div className="col-1">
+          <img className="d-block mx-auto" src={preficon} alt="Preferences" width="30" />
+        </div>
+        <div className="col-1">
+          <img className="d-block mx-auto" src={questionicon} alt="Info" width="30" />
+        </div>
+        <div className="col-8">
+          <p className="pageheader">Mix & Match</p>
+        </div>
+        <div className="col-2">
+          <img className="d-block mx-auto" src={usericon} alt="Profile" width="40" />
+        </div>
+      </div>
 
-		<div className="col-1">
-			<img className="d-block mx-auto" src={questionicon} alt="Info" width="30" />
-		</div>
+      {/* Shirt */}
+      <div className="row align-items-center my-2">
+        <div className="col">
+          <img className="left-arrow d-block mx-auto" src={leftarrow} alt="Prev Top" width="30" onClick={() => handlePrev('top')} />
+        </div>
+        <div className="col">
+          {tops.length > 0 && <img className="shirt-img d-block mx-auto" src={tops[topIndex].img} alt="Top" width="150" />}
+        </div>
+        <div className="col">
+          <img className="right-arrow d-block mx-auto" src={rightarrow} alt="Next Top" width="30" onClick={() => handleNext('top')} />
+        </div>
+      </div>
 
-		<div className="col-8">
-			<p className="pageheader">Mix & Match</p>
-			</div>
+      {/* Pant */}
+      <div className="row align-items-center my-2">
+        <div className="col">
+          <img className="left-arrow d-block mx-auto" src={leftarrow} alt="Prev Pant" width="30" onClick={() => handlePrev('pant')} />
+        </div>
+        <div className="col">
+          {pants.length > 0 && <img className="pant-img d-block mx-auto" src={pants[pantIndex].img} alt="Pant" width="150" />}
+        </div>
+        <div className="col">
+          <img className="right-arrow d-block mx-auto" src={rightarrow} alt="Next Pant" width="30" onClick={() => handleNext('pant')} />
+        </div>
+      </div>
 
-		<div className="col-2">
-			<img className="d-block mx-auto" src={usericon} alt="Profile" width="40" />
-		</div>
+      {/* Shoes */}
+      <div className="row align-items-center my-2">
+        <div className="col">
+          <img className="left-arrow d-block mx-auto" src={leftarrow} alt="Prev Shoe" width="30" onClick={() => handlePrev('shoe')} />
+        </div>
+        <div className="col">
+          {shoes.length > 0 && <img className="shoe-img d-block mx-auto" src={shoes[shoeIndex].img} alt="Shoe" width="150" />}
+        </div>
+        <div className="col">
+          <img className="right-arrow d-block mx-auto" src={rightarrow} alt="Next Shoe" width="30" onClick={() => handleNext('shoe')} />
+        </div>
+      </div>
 
-		</div>
-
-		{/*Mix & Match Screen Content*/}
-
-		{/*Shirt area*/}
-		<div className="row">
-			<div className="col">
-				<img className="d-block mx-auto left-arrow" src={leftarrow} alt="" width="30"></img>
-			</div>
-			<div className="col">
-				<img className="col shirt-img" src={Clothes.top1} alt="" width="150"></img>
-			</div>
-			<div className="col">
-				<img className="right-arrow" src={rightarrow} alt="" width="30"></img>
-			</div>
-		</div>
-
-		{/*Pant area*/}
-		<div className="row">
-			<div className="col">
-				<img className="d-block mx-auto left-arrow" src={leftarrow} alt="" width="30"></img>
-			</div>
-			<div className="col">
-				<img className="col shirt-img" src={Clothes.pant1} alt="" width="150"></img>
-			</div>
-			<div className="col">
-				<img className="right-arrow" src={rightarrow} alt="" width="30" ></img>
-			</div>
-		</div>
-
-		{/*Shoe area*/}
-		<div className="row">
-			<div className="col">
-				<img className="d-block mx-auto left-arrow" src={leftarrow} alt="" width="30"></img>
-			</div>
-
-			<div className="col shoe-img">
-				<img src={Clothes.shoe1} alt="" width="150"></img>
-			</div>
-
-			<div className="col">
-				<img className="right-arrow" src={rightarrow} alt="" width="30"></img>
-			</div>
-		</div>
-
-		{/* Bottom Navigation */}
-		<div className="row navbar fixed-bottom">
-	  	<div className="col text-center">
+      {/* Bottom Nav */}
+      <div className="row navbar fixed-bottom">
+        <div className="col text-center">
           <Link to="/" className="nav-link">
             <img src={hearticon} alt="Swipe" width="40" />
             <div>Swipe</div>
           </Link>
         </div>
-
         <div className="col text-center">
           <Link to="/closet" className="nav-link">
             <img src={closeticon} alt="Closet" width="40" />
@@ -111,9 +125,7 @@ const MixMatch = () => {
           </Link>
         </div>
       </div>
-	</div>
-		
-    
+    </div>
   );
 };
 
